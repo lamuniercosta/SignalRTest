18e9ec7e662d13756e83cf4d5ae802aadf26e6fe
Lamunier Costa dos Santos <lamunier.costa@gmail.com>
2017-08-15 15:21:30 -0300

    Commit inicial do projeto

---
+++ SignalRTest/SignalRTest/Hubs/Chat.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRTest.Hubs
+{
+    public class Chat : Hub
+    {
+        public void EnviarMensagem(string apelido, string mensagem)
+    {
+        Clients.TransmitirMensagem(apelido, mensagem);
+    }
+
+}
+}
